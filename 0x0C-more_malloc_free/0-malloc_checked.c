cf37bc72ba3a6b1201d1ed8812def7815d144559
JosephRMacharia <112111585+JosephRMacharia@users.noreply.github.com>
2022-10-06 23:01:07 +0300

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,9 +3,8 @@
 
 /**
  * malloc_checked - Allocates memory using malloc.
- * @b: The number of bytes to be allocated.
- *
- * Return: A pointer to the allocated memory.
+ * @b: The number of bytes to be allocated
+ * Return: A pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
